0e3d7b79120d5c76297e0c03d89eae9deb78ebef
Jon Evans <Jevans2497@gmail.com>
2020-10-04 15:01:23 -0600

    Figured out game logic

--- Desktop/thisgameisunderrated/server/tgiu-game.js
+++ Desktop/thisgameisunderrated/server/tgiu-game.js
@@ -19,7 +19,6 @@ class tgiuGame {
 	}
 
 	buttonClicked(button) {
-		//Tell client to disable buttons
 		if (this.allPlayersHaveChosen()) {
 			this.showResults();
 		}
@@ -36,8 +35,35 @@ class tgiuGame {
 	}
 
 	showResults() {
-		this.getNewWord();
-		this.loadWord();
+		var countArr = this.countPlayerChoices();
+		var resultsArr = this.convertCountsToResults(countArr);
+		console.log(resultsArr);
+		//Reset buttons
+		// this.getNewWord();
+		// this.loadWord();
+	}
+
+	countPlayerChoices() {
+		var underratedCount = this.players.filter((p) => p.buttonSelected == "underrated").length;
+		var properlyCount = this.players.filter((p) => p.buttonSelected == "properlyrated").length;
+		var overratedCount = this.players.filter((p) => p.buttonSelected == "overrated").length;
+		return [underratedCount, properlyCount, overratedCount];
+	}
+
+	convertCountsToResults(countArr) {
+		var resultsArr = [];
+		var min = Math.min(...countArr.filter((e) => e != 0)); //Min excluding 0
+		var index;
+		for (index = 0; index < countArr.length; index++) {
+			if (countArr[index] == 0) {
+				resultsArr.push(0);
+			} else if (countArr[index] == min) {
+				resultsArr.push(-1);
+			} else {
+				resultsArr.push(1);
+			}
+		}
+		return resultsArr;
 	}
 
 	loadWord() {
@@ -59,7 +85,7 @@ class tgiuGame {
 	}
 
 	makeWords() {
-		this.words = ['random1', 'list2', 'of3', 'words4', 'boop5'];
+		this.words = ['Pirates', 'Iced tea', 'The office', 'First few minutes of waking up', 'Whisky', 'Mohawks', 'Kanye', 'Quarantine', 'Spanish', 'Winter', 'Mint ice cream', 'Six pack abs', 'The moon', 'Books you read in highschool', 'The number 7', 'Smoothies', 'Finding Nemo', 'Disney world', 'Movie theatres', 'Subway', 'Tom Hanks', 'New York', 'Ghost stories', 'Campfires', 'Foosball', 'Sunrise', 'Facebook', 'The seven deadly sins', 'The Olympics', 'Scavenger hunts', 'John Mulaney', 'Italy', 'Thanksgiving', 'Dinosaurs', 'Youtube', 'French kissing', 'Friends (TV show)', 'Purple', 'Cupcakes', 'Plastic surgery', 'Harry Potter', 'Air travel', 'Road trips', 'Chex mix', 'Punk music', 'Yoga', 'Star wars', 'Harvard', 'Horseback riding', 'Turtlenecks', 'Apple (company)', 'Hot dogs', 'Mirrors', 'Trail mix', 'Pigeons', 'Pokemon', 'Marvel movies', 'Gatorade', 'Frozen pizza'];
 		this.shuffle(this.words);
 	}
 
