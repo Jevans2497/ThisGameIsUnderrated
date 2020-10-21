b1ac70678b49ed9290fd8f13355a080cc6da6591
Jon Evans <Jevans2497@gmail.com>
2020-10-20 20:32:47 -0600

    test

---
+++ client.js
@@ -0,0 +1,175 @@
+const setNewWord = (text) => {
+	const parent = document.querySelector('#wordEvent');
+	parent.innerHTML = '';
+	const el = document.createElement('h1');
+	el.innerHTML = text;
+	parent.appendChild(el);
+}
+
+const addButtonListeners = () => {
+	['underrated', 'properlyrated', 'overrated'].forEach((id) => {
+		const button = document.getElementById(id);
+		button.addEventListener('click', () => {
+			socket.emit('buttonClicked', id);
+		});
+	});
+}
+
+const addToQueueButtonListener = () => {
+	const addToQueueButton = document.getElementById("queue-button");
+	addToQueueButton.addEventListener('click', () => {
+		var queueTextNode = document.getElementById("queue-text");
+		socket.emit('addToQueue', queueTextNode.value);	
+		queueTextNode.value = "";
+		addToQueueButton.style.backgroundColor = "#e7e7e7";
+		addToQueueButton.disabled = true;
+	});
+}
+
+const displayResults = (resultsArr) => {
+	var ids = ['underrated', 'properlyrated', 'overrated'];
+	var i;
+	for (i = 0; i < ids.length; i += 1) {
+		const button = document.getElementById(ids[i]);
+		if (resultsArr[i] == -1) {
+			button.style.background = '#e6e6e6';
+			button.style.color = '#bfbfbf';
+		} else if (resultsArr[i] == 0) {
+			button.style.background = '#ffb3b3';
+			button.style.color = '#000000';
+		} else {
+			button.style.background = '#99ff99';
+			button.style.color = '#000000';
+		}
+		button.disabled = true;
+	}
+}
+
+const resetResults = () => {
+	var ids = ['underrated', 'properlyrated', 'overrated'];
+	var i;
+	for (i = 0; i < ids.length; i += 1) {
+		const button = document.getElementById(ids[i]);
+		button.style.background = '#e7e7e7';
+		button.style.color = '#000000';
+		button.style.boxShadow = "none";
+		button.disabled = false;
+	}
+}
+
+var newWordHasEventListener = false;
+
+const showNewWordButton = () => {
+	const newWordButton = document.getElementById("new-word-button");
+	newWordButton.style.backgroundColor = "#cef3ff";
+	newWordButton.style.color = "#000000";
+	newWordButton.disabled = false;
+	if (!newWordHasEventListener) {
+		newWordButton.addEventListener('click', () => {
+			socket.emit('newRound');
+		});
+		newWordHasEventListener = true;
+	}
+}
+
+const resetNewWordButton = () => {
+	const newWordButton = document.getElementById("new-word-button");
+	newWordButton.style.backgroundColor = "transparent";
+	newWordButton.style.color = "transparent";
+	newWordButton.disabled = true;
+}
+
+const displayNames = (playersChoicesDict) => {
+	console.log(playersChoicesDict);
+	for (let id in playersChoicesDict) {
+		let listId = getListIdFromButtonId(id);
+		for (var name of playersChoicesDict[id]) {
+			addNameToList(listId, name);
+		}
+	}
+}
+
+const resetNames = () => {
+	var ids = ["underList", "properlyList", "overList"];
+	ids.forEach((id) => {
+		const list = document.getElementById(id);
+		list.innerHTML = "";
+	});
+}
+
+const getListIdFromButtonId = (buttonId) => {
+	switch (buttonId) {
+		case "underrated":
+			return "underList";
+		case "properlyrated":
+			return "properlyList";
+		case "overrated":
+			return "overList";
+		default:
+			return "ERROR";
+		}
+}
+
+const addNameToList = (listId, name) => {
+	parent = document.getElementById(listId);
+	const li = document.createElement("li");
+	li.classList.add("button-list")
+	li.innerHTML = name;
+	parent.appendChild(li);
+}
+
+const resetGame = () => {
+	resetResults();
+	resetNewWordButton();
+	resetNames();
+	resetDecidedPlayers();
+}
+
+const updateSidebarNamesList = (playerNames) => {
+	resetSidebarNameList();
+	var thisPlayersName = sessionStorage.name
+	playerNames.sort(function(x,y){ return x == thisPlayersName ? -1 : y == thisPlayersName ? 1 : 0; });
+	const parent = document.getElementById("name-list");
+	playerNames.forEach((name) => {
+		const li = document.createElement("li");
+		li.innerHTML = name;
+		li.classList.add("opponent-name");
+		parent.appendChild(li);
+	});
+}
+
+const resetSidebarNameList = () => {
+	var sideBarList = document.getElementById("name-list");
+	sideBarList.innerHTML = "";
+}
+
+const showDecidedPlayers = (decidedPlayers) => {
+	let sideBarList = document.getElementById("name-list");
+	sideBarList.childNodes.forEach((child) => {
+		if (decidedPlayers.includes(child.innerHTML)) {
+			child.style.color = "#000000";
+		}
+	});
+}
+
+const resetDecidedPlayers = () => {
+	let sideBarList = document.getElementById("name-list");
+	sideBarList.childNodes.forEach((child) => {
+		child.style.color = "#a3a3a3";
+	});
+}
+
+const socket = io.connect();
+socket.on('newWord', setNewWord);
+socket.on('results', displayResults);
+socket.on('nameAndShame', displayNames);
+socket.on('showNewWordButton', showNewWordButton);
+socket.on('reset', resetGame);
+socket.on('updateNamesList', updateSidebarNamesList)
+socket.on('playerDecided', showDecidedPlayers)
+socket.emit('joinRoom', sessionStorage.roomName.toUpperCase());
+socket.emit('name', sessionStorage.name);
+
+
+addButtonListeners();
+addToQueueButtonListener();
---
+++ preProcessor.py
@@ -0,0 +1,327 @@
+#Just needed some way to get the list from my phone to a javascript ready line of code
+list = """[  ] Pirates
+[  ] Iced Tea
+[  ] The Office
+[  ] First Few Minutes of Waking Up
+[  ] Whisky
+[  ] Mohawks
+[  ] Kanye
+[  ] Quarantine
+[  ] Spanish
+[  ] Winter
+[  ] Mint ice cream
+[  ] Six Pack Abs
+[  ] The Moon
+[  ] Books you Read in Highschool
+[  ] The Number 13
+[  ] Smoothies
+[  ] Finding Nemo
+[  ] Disney World
+[  ] Movie Theatres
+[  ] Subway
+[  ] Tom Hanks
+[  ] New York
+[  ] Ghost stories
+[  ] Campfires
+[  ] Foosball
+[  ] Sunrise
+[  ] Facebook
+[  ] The 7 Deadly Sins
+[  ] The Olympics
+[  ] Scavenger Hunts
+[  ] John Mulaney
+[  ] Italy
+[  ] Thanksgiving
+[  ] Dinosaurs
+[  ] Youtube
+[  ] French kissing
+[  ] Friends (TV show)
+[  ] Purple
+[  ] Cupcakes
+[  ] Plastic Surgery
+[  ] Harry Potter
+[  ] Air Travel
+[  ] Road Trips
+[  ] Chex Mix
+[  ] Punk Music
+[  ] Yoga
+[  ] Star Wars
+[  ] Harvard
+[  ] Horseback Riding
+[  ] Turtlenecks
+[  ] Apple (company)
+[  ] Hot Dogs
+[  ] Mirrors
+[  ] Trail Mix
+[  ] Pigeons
+[  ] Pokemon
+[  ] Marvel Movies
+[  ] Gatorade
+[  ] Frozen pizza
+[  ] The Beatles
+[  ] The name "Katie"
+[  ] The Constitution
+[  ] IKEA
+[  ] Lighthouses
+[  ] Live Music
+[  ] Love
+[  ] The Beach
+[  ] Top Hats
+[  ] Gravy
+[  ] Johnny Appleseed
+[  ] Going as a Cat for Halloween
+[  ] Times New Roman
+[  ] Bears
+[  ] Beets
+[  ] SNL
+[  ] Optical Illusions
+[  ] Kaleidescopes
+[  ] Baseball
+[  ] Mayonnaise
+[  ] Monopoly
+[  ] Marijuana
+[  ] Mosquitos
+[  ] Silver
+[  ] Jeopardy
+[  ] August
+[  ] The Macarena
+[  ] Cruises
+[  ] The Search for Big Foot
+[  ] The Bachelor
+[  ] Cards Against Humanity
+[  ] Costco
+[  ] Taco Bell
+[  ] Blue Eyes
+[  ] Haircuts
+[  ] Trampolines
+[  ] Skiing / Snowboarding
+[  ] Jupiter
+[  ] Emojis
+[  ] The 1990s
+[  ] Britney Spears
+[  ] Exploring Space
+[  ] Elon Musk
+[  ] Overalls
+[  ] Jean Shorts
+[  ] Constellations
+[  ] Canada
+[  ] Ballet
+[  ] Oatmeal Raisin Cookies
+[  ] Peanut Butter
+[  ] Julius Caesar
+[  ] Owning a Home
+[  ] Feudalism
+[  ] Proper Grammar.
+[  ] Time Travel
+[  ] Mullets
+[  ] Werewolves
+[  ] Highschool
+[  ] College
+[  ] Balconies
+[  ] Pharmaceutical Drug Commercials
+[  ] Indian Curry
+[  ] Ketchup
+[  ] Classical Music
+[  ] Hawaii
+[  ] Crying
+[  ] Axe Body Spray
+[  ] Sleeping on a Couch
+[  ] Rain
+[  ] Marriage
+[  ] Mouthwash
+[  ] Camp Songs
+[  ] Trap Music
+[  ] Lebron James
+[  ] The Word "Bro"
+[  ] Soccer
+[  ] Calling Soccer "Futbol"
+[  ] Snoop Dogg
+[  ] Hard Seltzer
+[  ] Roundabouts (Driving)
+[  ] Minecraft
+[  ] Participation Trophies
+[  ] Polaroid Cameras
+[  ] Microwave Meals
+[  ] Asparagus
+[  ] Breaking Bad
+[  ] Dating Apps
+[  ] Borat
+[  ] Karate
+[  ] The Japanese Flag
+[  ] Meaningless Tattoos
+[  ] Staring Contests
+[  ] Shark Tooth Necklaces
+[  ] Rock Paper Scissors
+[  ] Sparknotes
+[  ] Bumper Stickers
+[  ] Truck Stops
+[  ] Watches
+[  ] Flip Flops
+[  ] Vice Presidents
+[  ] Slinkys
+[  ] Wildlife
+[  ] Electric Cars
+[  ] Podcasts
+[  ] Kayaking
+[  ] Spoons
+[  ] Inspirational Quotes
+[  ] Coasters
+[  ] Roller Coasters
+[  ] Volcanoes
+[  ] Art Museums
+[  ] Fairy Tales
+[  ] Pepsi
+[  ] Vinyl
+[  ] Candles
+[  ] Hamilton
+[  ] Hummus
+[  ] Ping Pong
+[  ] Billiards
+[  ] Chess
+[  ] Cheese
+[  ] Guitar Hero
+[  ] Snapchat Filters
+[  ] Music Festivals
+[  ] Thrift Stores
+[  ] Waffles
+[  ] Pancakes
+[  ] Running a Marathon
+[  ] Coffee
+[  ] The American Midwest
+[  ] Gardening
+[  ] Archery
+[  ] Swimming in a Lake
+[  ] Hard-Cover Books
+[  ] Llamas
+[  ] Lord of the Rings
+[  ] Improv Comedy
+[  ] American Idol
+[  ] Bowling
+[  ] Taking a Bath
+[  ] Twitter
+[  ] Babies
+[  ] Graduation Ceremonies
+[  ] Car Commercials
+[  ] AC/DC
+[  ] Reading
+[  ] Jogging
+[  ] Social Media
+[  ] Cats
+[  ] Dogs
+[  ] Pet Fish
+[  ] Zoos
+[  ] Aquariums
+[  ] Amusement parks
+[  ] Toast
+[  ] Rye Bread
+[  ] Breakfast Cereal
+[  ] Bottled Water
+[  ] Pulling an All-Nighter
+[  ] Vegetables
+[  ] Fruits
+[  ] Carrots
+[  ] Onions
+[  ] Tomatoes
+[  ] Bananas
+[  ] Pineapples
+[  ] Apples
+[  ] Pears
+[  ] Cucumbers
+[  ] Hotels
+[  ] Birthdays
+[  ] Colored Pencils
+[  ] Watching TV
+[  ] Alcohol
+[  ] Sleeping
+[  ] Refrigerators
+[  ] Water Parks
+[  ] Computers
+[  ] Headphones
+[  ] Swimming Pools
+[  ] Hiking
+[  ] Saran Wrap
+[  ] Forks
+[  ] The Sun
+[  ] The Moon
+[  ] Trick or treating
+[  ] Febreeze
+[  ] Motorcycles
+[  ] Tacos
+[  ] Vitamins
+[  ] Starbucks
+[  ] Jazz
+[  ] Single-use Plastics
+[  ] Crossing the Country in an RV
+[  ] Greek Mythology
+[  ] Nascar
+[  ] Roses
+[  ] Shakespeare
+[  ] Hide and Seek
+[  ] Popcorn
+[  ] Craft Beer
+[  ] Fishing
+[  ] Love Seat
+[  ] Donuts
+[  ] The Grateful Dead
+[  ] Corn
+[  ] Deep Dish Pizza
+[  ] Sequels
+[  ] The Internet
+[  ] Tennis
+[  ] The Eiffel Tower
+[  ] The Statue of Liberty
+[  ] Fraternities and Sororities
+[  ] Nickelback
+[  ] Abraham Lincoln
+[  ] George W. Bush
+[  ] Dreams
+[  ] Jewelry
+[  ] Pumpkin Spice
+[  ] Antique Shops
+[  ] Fanny Packs
+[  ] Edward Snowden
+[  ] Summer
+[  ] Autumn
+[  ] Spring
+[  ] Yellow Gatorade
+[  ] Red Gatorade
+[  ] Tylenol
+[  ] Salmon
+[  ] The Grand Canyon
+[  ] Hot Tubs
+[  ] The Only Thing We Have to Fear is Fear Itself
+[  ] George Washington
+[  ] Spiders
+[  ] Umbrellas
+[  ] Carving Pumpkins
+[  ] Perseverance
+[  ] Giraffes
+[  ] Courage
+[  ] Brunch
+[  ] Breakfast
+[  ] Memes
+[  ] Uber/Lyft
+[  ] Shrek
+[  ] Aviator Sunglasses
+[  ] Sailing
+[  ] Magic Tricks
+[  ] Roller Blading
+[  ] Ice Skating
+[  ] Coconut Water
+[  ] Dr. Suess Books
+[  ] Vampire Weekend
+[  ] Mini Golf
+[  ] Getting a Drivers License
+[  ] Southern Accents
+[  ] Mr. Brightside
+[  ] Shel Silverstein
+[  ] Poetry
+[  ] Curse Words
+[  ] Pig Latin
+[  ] British Accents"""
+
+splitList = list.split("[  ]")
+strippedList = map(str.strip, splitList)
+joinedList = '\', \''.join(strippedList)
+print(joinedList)
+
